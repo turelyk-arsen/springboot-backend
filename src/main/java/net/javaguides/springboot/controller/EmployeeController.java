0502f3c718291ae44f2fef40d78929973c52fd08
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-07-03 16:41:21 +0200

    all

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -1,8 +1,14 @@
 package net.javaguides.springboot.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +31,37 @@ public class EmployeeController {
 	public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
 		return new ResponseEntity<Employee>
 		(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+	}
+	
+	//build get all employees REST API
+	@GetMapping
+	public List<Employee> getAllEmployees(){
+		return employeeService.getAllEmployees();
+	}
+	
+	//build get employee by ID REST API
+	//http://localhost:8080/api/employee/1
+	@GetMapping("{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long employeeId) {
+		return new ResponseEntity<Employee>(employeeService.getEmployeeById(employeeId), HttpStatus.OK);
+	}
+	
+	// build update employee
+	//http://localhost:8080/api/employee/1
+	@PutMapping("{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long id,
+			                                       @RequestBody Employee employee){
+		return new ResponseEntity<Employee>(employeeService.updateEmployee(employee, id), HttpStatus.OK);
+	}
+	
+	// build delete REST API
+	//http://localhost:8080/api/employee/1
+	@DeleteMapping("{id}")
+	public ResponseEntity<String> deleteEmployee(@PathVariable("id") long id){
+		//delete employee from DB
+		employeeService.deleteEmployee(id);
+		return new ResponseEntity<String>("Employee deleted successfully!", HttpStatus.OK);
+		
 	}
 
 }
